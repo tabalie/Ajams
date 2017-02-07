f952aa58221228ddeb8b6a13c017531064226ed0
Natalie Kueneman <tabalie@gmail.com>
2017-02-07 14:34:32 -0700

    CP5: Controllers - completed.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,7 @@
 
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
             })
 
