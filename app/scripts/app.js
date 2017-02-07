7db93c02dfce79eb023cc7e043b8296b41b7a37b
Natalie Kueneman <tabalie@gmail.com>
2017-02-07 13:34:43 -0700

    CP3: Routing and States - completed.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,31 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            //to display clean URLs to user without hashbang
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+
+        //to allow ui-view to load template associated with landing, album, and collection states
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+
+            .state('collection', {
+                url: '/',
+                templateUrl: '/templates/collection.html'
+            });
+    }
+
+    angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
