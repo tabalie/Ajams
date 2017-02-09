f9d1c80edf288979bee5aaa55613897b2abe2b59
Natalie Kueneman <tabalie@gmail.com>
2017-02-09 16:22:56 -0700

    Worked on adding autoplay feature, progress.

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -40,7 +40,8 @@
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
-                preload: true
+                preload: true,
+                // autoplay: true
             });
 
             currentBuzzObject.bind('timeupdate', function() {
